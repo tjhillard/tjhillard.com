da1966b423b88c22b345d3d3abba7c970430bb5f
tjhillard <tjhillard@gmail.com>
2019-08-21 20:05:24 -0700

    update GA config

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,6 +15,9 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         trackingId: process.env.GOOGLE_ANALYTICS_TRACKING_ID,
+        head: false,
+        anonymize: true,
+        respectDNT: true,
       },
     },
     {
